3addb2150429c39b0b56a195c2dcc53db01d49d4
Federico Gorosito <federico.a.gorosito@gmail.com>
2022-10-26 15:41:16 -0300

    .

--- js/script.js
+++ js/script.js
@@ -19,11 +19,11 @@ function menuPrincipal (menu){
     let productos = Number( prompt("Para comprar un teclado 100% ingrese 1, Para comprar un teclado 75% ingrese 2, Para comprar un teclado 60% ingrese 3"));
 
     if (productos == 1) {
-        console.log("compraste teclado 100%, su valor es de $" + teclado100Precio);
+        alert("compraste teclado 100%, su valor es de $" + teclado100Precio);
     } else if (productos == 2) {
-        console.log("compraste teclado 75%, su valor es de $" + teclado75Precio);
+        alert("compraste teclado 75%, su valor es de $" + teclado75Precio);
     } else if (productos == 3)
-    console.log("compraste teclado 60%, su valor es de $" + teclado60Precio);
+        alert("compraste teclado 60%, su valor es de $" + teclado60Precio);
     else{
         menuPrincipal(alert("No elegiste nada"));
     };
