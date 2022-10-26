12c453c728b37eba08d689060937aa632f2ec1fe
Federico Gorosito <federico.a.gorosito@gmail.com>
2022-10-25 22:59:13 -0300

    modifico js

--- js/script.js
+++ js/script.js
@@ -1,19 +1,94 @@
-let teclados1 = "teclado 100% " + "$" + 18000; 
-let teclados2 = "teclado 75% " + "$" + 12000;
-let teclados3 = "teclado 60% " + "$" + 10000;  
+function bienvenida (usuario) {
+    alert("Hola " + usuario + " bienvenido a GoroCaps" )
+}
 
-let bienvenida = alert("Bienvidos a GoroCaps")
+bienvenida(prompt("Ingrese su nombre"))
 
-let compras = prompt("Para comprar un teclado presione 1, para comprar un keycap presione 2, para comprar un switch presione 3")
+menuPrincipal()
 
-if(compras == 1){
-    console.log(teclados1)
-}else if (compras == 2){
-    console.log("keycaps")
-}else if (compras == 3){
-    console.log("switchs")
-}else{
-    console.log("ingreso mal el numero")
+function menuPrincipal (menu){
+    let productos = Number( prompt("Para comprar un teclado ingrese 1, Para comprar un switch ingrese 2, Para comprar un keycap ingrese 3"))
+
+let tiposTeclados 
+let tiposSwitchs
+let tiposKeycaps
+let teclado100
+
+
+switch (productos) {
+    case 1:
+        tiposTeclados = Number( prompt("Para comprar teclado 100% ingrese 1, Para comprar teclado 75% ingrese 2,Para comprar teclado 60% ingrese 3"))
+
+        switch (tiposTeclados) {
+            case 1:
+                teclado100 = prompt ("El teclado 100% vale $ 18.000 desea comprarlo? si o no")
+
+                if (teclado100 === "si" || "Si" || "SI") {
+                    alert("compraste teclado 100%")
+                }else {
+                    menuPrincipal()
+                }
+            break;
+        
+            case 2:
+                console.log ("El teclado 75% vale $12.000")
+            break;
+        
+            case 3:
+                console.log("El teclado 60% vale $10.000")
+            break;
+        
+            default:
+                menuPrincipal(prompt("Ingresó mal el número, para continuar presione cualquier tecla y enter"))
+                break;
+        }
+        
+    break;
+
+    case 2:
+        tiposSwitchs = Number( prompt("Para comprar un switch brown ingrese 1, Para comprar un switch blue ingrese 2,Para comprar un switch red ingrese 3"))
+
+        switch (tiposSwitchs) {
+            case 1:
+                console.log("El swicth brown vale $3.500 el pack de 20 unidades")
+            break;
+        
+            case 2:
+                console.log("El swicth blue vale $2.500 el pack de 20 unidades")
+            break;
+        
+            case 3:
+                console.log("El swicth brown vale $2.900 el pack de 20 unidades")
+            break;
+        
+            default:
+                menuPrincipal(prompt("Ingresó mal el número, para continuar presione cualquier tecla y enter"))
+            break;
+        }
+    break;
+
+    case 3:
+        tiposKeycaps = Number( prompt("Para comprar los keycaps customs 3D ingrese 1, Para comprar los keycaps personalizados ingrese 2"))
+
+        switch (tiposKeycaps) {
+            case 1:
+                console.log("Los keycaps 3D valen $2.200 por unidad")
+            break;
+        
+            case 2:
+                console.log("Los keycaps personalizados valen $1.000 por unidad")
+            break;
+            
+            default:
+                console.log("Ingresó mal el número")
+            break;
+        }
+    break;
+
+    default:
+        menuPrincipal(prompt("Ingresó mal el número, para continuar presione cualquier tecla y enter"))
+        break;
+}
 }
 
 
